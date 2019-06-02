74dbefb051d520a36612ef14118e70735aa4a225
karo <wilzilzor@gmail.com>
2019-06-02 23:12:22 +0900

    first commit

---
+++ src/test/scala/day1/test.scala
@@ -0,0 +1,17 @@
+package day1
+
+import cats._, cats.data._, cats.implicits._
+import org.scalatest.{FlatSpec, FunSpec, Matchers}
+
+class test extends FunSpec with Matchers {
+
+  describe("HI") {
+    describe("") {
+      it(""){
+        1 === "TRUE" // type mismatch error
+        1 == "TRUE" // always false 컴파일 에러가 나지 않음
+
+      }
+    }
+  }
+}
