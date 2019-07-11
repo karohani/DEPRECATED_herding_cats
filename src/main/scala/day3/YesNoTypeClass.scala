dedd6600ea18ddd5699ae23237f2771ea0f661bc
karo <wilzilzor@gmail.com>
2019-07-12 08:38:24 +0900

    squash! modify build.sbt

---
+++ src/main/scala/day3/YesNoTypeClass.scala
@@ -0,0 +1,18 @@
+package day3
+
+import simulacrum._
+
+@typeclass trait CanTruthly[A] {
+  self =>
+  def truthly(a: A): Boolean
+}
+
+object CanTruthly {
+  def fromTruth[A](f: A => Boolean): CanTruthly[A] = new CanTruthly[A] {
+    override def truthly(a: A): Boolean = f(a)
+  }
+}
+
+class YesNoTypeClass {
+
+}
---
+++ src/test/scala/day3/ApplicativeTests.scala
@@ -0,0 +1,45 @@
+package day3
+
+import org.scalatest.{FlatSpec, FunSpec, Matchers}
+import cats._, cats.data._, cats.implicits._
+
+class ApplicativeTests extends FunSpec with Matchers {
+
+  describe("HI") {
+    describe("") {
+      it("") {
+        val hs = Functor[List].map(List(1, 2, 3, 4))({
+          (_: Int) * (_: Int)
+        }.curried)
+
+
+        val a = {
+          (_: Int) * (_: Int)
+        }.curried
+        println(a(1)); //scala.Function2$$Lambda$143/960733886@157853da
+        println(a(1)(2))
+        // currying 된 펑션을 만들어준다.
+      }
+    }
+  }
+  describe("Applicative") {
+    it("") {
+      val F = Applicative[Option]
+      val what: Option[Int => Int] = F.pure((_: Int) + 3)
+      val b: Option[Int] = F.ap({
+        F.pure((_: Int) + 3)
+      })(F.pure(9))
+    }
+    it("Util Function") {
+      def sequenceA[F[_] : Applicative, A](list: List[F[A]]): F[List[A]] = list match {
+        case Nil => Applicative[F].pure(Nil: List[A])
+        case x :: xs => (x, sequenceA(xs)) mapN {
+          _ :: _
+        }
+      }
+
+      println(sequenceA(List(1.some, 2.some)))
+    }
+  }
+}
+
