5b0014d8118b4af2a0d25ce73c73d89780d26cb8
karo <wilzilzor@gmail.com>
2019-08-06 22:04:28 +0900

    ˆ

---
+++ src/test/scala/chapter3/FunctorIn.scala
@@ -0,0 +1,24 @@
+package chapter3
+
+import org.scalatest.FlatSpec
+import scala.language.higherKinds
+import cats.Functor
+import cats.instances.list._
+import cats.instances.option._
+
+class FunctorIn extends FlatSpec {
+  "test" should "aaa" in {
+    val list1 = List(1, 2, 3)
+    val list2 = Functor[List].map(list1)(j => j * 2)
+    print(list2)
+
+    import cats.instances.function._
+    import cats.syntax.functor._
+    def doMath[F[_]](start: F[Int])(implicit functor: Functor[F]): F[Int] = start.map(n => n + 1 * 2)
+
+    import cats.instances.option._
+    print(doMath(Option(20)))
+    // SOME(22)
+  }
+
+}
